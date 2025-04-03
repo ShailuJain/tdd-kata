4712d2672ff26a1d53e64a85cfcca0d355746bd1
Shailesh Jain <jainshailesh91@gmail.com>
2025-04-03 15:39:14 +0000

    add case to handle multiple numbers separated by ,

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,13 +3,18 @@ require_relative '../string_calculator'
 RSpec.describe StringCalculator do
   describe "#add" do
     it "returns 0 for an empty string" do
-      calculator = StringCalculator.new
-      expect(calculator.add("")).to eq(0)
+        calculator = StringCalculator.new
+        expect(calculator.add("")).to eq(0)
     end
 
     it "returns the number itself for a single number string" do
         calculator = StringCalculator.new
         expect(calculator.add("1")).to eq(1)
-      end
+    end
+
+    it "returns the sum of two numbers" do
+        calculator = StringCalculator.new
+        expect(calculator.add("1,2")).to eq(3)
+    end
   end
 end
