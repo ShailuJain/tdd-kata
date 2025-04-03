18e9b9ba55363ae989416eb1f3594d4d7727ab41
Shailesh Jain <jainshailesh91@gmail.com>
2025-04-03 15:56:07 +0000

    handle custom delimeter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,10 +21,15 @@ RSpec.describe StringCalculator do
         calculator = StringCalculator.new
         expect(calculator.add("5,1,3,4")).to eq(13)
     end
-    
+
     it "handles new lines between numbers" do
         calculator = StringCalculator.new
         expect(calculator.add("1\n2,3")).to eq(6)
+    end
+
+    it "supports custom delimiter" do
+        calculator = StringCalculator.new
+        expect(calculator.add("//;\n1;2")).to eq(3)
     end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
     def add(numbers)
-      return 0 if numbers.empty?
-      numbers.split(/,|\n/).map(&:to_i).sum
+        return 0 if numbers.empty?
+        delimiter = /,|\n/
+        if numbers.start_with?("//")
+            parts = numbers.split("\n", 2)
+            delimiter = Regexp.escape(parts[0][2])
+            numbers = parts[1]
+        end
+        numbers.split(/#{delimiter}/).map(&:to_i).sum
     end
 end
